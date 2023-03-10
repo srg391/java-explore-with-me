f8811edaba80efc3dcaa44149d372532c78f5f4e
Sergei <srg391@yandex.ru>
2023-03-11 00:24:56 +0300

    fix: добавить сортировку

--- stats/stats-server/src/main/java/ru/practicum/StatsRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/StatsRepository.java
@@ -10,9 +10,9 @@ import java.util.List;
 
 public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
 
-    @Query("SELECT new ru.practicum.model.ViewStats(e.app, e.uri, COUNT (DISTINCT e.ip)) from EndpointHit e WHERE e.timestamp> ?1 AND e.timestamp< ?2 GROUP BY e.app, e.uri")
+    @Query("SELECT new ru.practicum.model.ViewStats(e.app, e.uri, COUNT (DISTINCT e.ip)) from EndpointHit e WHERE e.timestamp> ?1 AND e.timestamp< ?2 GROUP BY e.app, e.uri ORDER BY COUNT (e.ip) DESC")
     List<ViewStats> findAllUnique(LocalDateTime startCod, LocalDateTime endCod, List<String> uris, boolean unique);
 
-    @Query("SELECT new ru.practicum.model.ViewStats(e.app, e.uri, COUNT (e.ip)) from EndpointHit e WHERE e.timestamp> ?1 AND e.timestamp< ?2 GROUP BY e.app, e.uri")
+    @Query("SELECT new ru.practicum.model.ViewStats(e.app, e.uri, COUNT (e.ip)) from EndpointHit e WHERE e.timestamp> ?1 AND e.timestamp< ?2 GROUP BY e.app, e.uri ORDER BY COUNT (e.ip) DESC")
     List<ViewStats> findAll(LocalDateTime start, LocalDateTime end, List<String> uris);
 }
